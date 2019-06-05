1b1fe89573bf41e0d6f37584b05f7e2e94c692ef
Alienigenaro <32861360+Alienigenaro@users.noreply.github.com>
2019-06-05 16:06:50 -0300

    Create picalc.c

---
+++ picalc.c
@@ -0,0 +1,40 @@
+
+
+
+//Aqui vão ficar os algoritmos de calcular o pi
+
+//Usar Posix Pthreads para multithread
+//Utilizar a biblioteca GMP para manipulação de big numbers
+
+//Isso significa que a gente tem que pesquisar algoritmos prontos que utilizem essas bibliotecas...
+
+//https://shanetully.com/2012/11/calculating-pi-to-10000000-digits-with-mpfr-and-threads/
+
+void montecarlo(){
+
+
+
+}
+
+void gaussLegendre(){
+  //http://codepad.org/xD1djgRG
+  //Adaptar o código pra usar o GMP e o posix pthreads
+
+
+}
+
+
+void borwein(){
+  
+
+}
+
+
+
+
+
+
+
+
+
+
