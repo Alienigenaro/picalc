cda5d9d6e76e4f80f40d1f18bfb51e7337694ec0
Alienigenaro <32861360+Alienigenaro@users.noreply.github.com>
2019-06-11 16:11:29 -0300

    Create pi.h

---
+++ pi.h
@@ -0,0 +1,10 @@
+#ifndef PI_H
+#define PI_H
+
+#include <math.h>
+#include <gmp.h>
+
+//Protótipo das funções
+void Borwein(unsigned long int it,int th, unsigned long int pr,mpf_t  retval);
+
+#endif
